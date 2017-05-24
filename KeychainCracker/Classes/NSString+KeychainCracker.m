0b23e468d96fdd9e1f3ceeade6aca1391bf9d9a8
macmade <macmade@xs-labs.com>
2017-05-24 21:02:46 +0200

    Objective-C - Common substitutions...

--- KeychainCracker/Classes/NSString+KeychainCracker.m
+++ KeychainCracker/Classes/NSString+KeychainCracker.m
@@ -29,6 +29,8 @@
 
 #import "NSString+KeychainCracker.h"
 
+static NSMutableDictionary< NSString *, NSArray< NSString * > * > * variants = nil;
+
 @implementation NSString( KeychainCracker )
 
 - ( NSArray< NSString * > * )caseVariants
@@ -39,7 +41,7 @@
     NSUInteger                     i;
     NSUInteger                     j;
     NSUInteger                     n;
-    NSMutableArray< NSString * > * variants;
+    NSMutableArray< NSString * > * v;
     
     if( self.length == 0 )
     {
@@ -61,7 +63,7 @@
         return @[ self ];
     }
     
-    variants = [ NSMutableArray new ];
+    v = [ NSMutableArray new ];
     
     for( i = 0, n = ( NSUInteger )pow( 2, length ); i < n; i++ )
     {
@@ -70,17 +72,101 @@
             permutation[ j ] = ( ( i >> j & 1 ) != 0 ) ? ( char )toupper( cp[ j ] ) : cp[ j ];
         }
         
-        [ variants addObject: [ NSString stringWithUTF8String: permutation ] ];
+        [ v addObject: [ NSString stringWithUTF8String: permutation ] ];
     }
     
     free( permutation );
     
-    return variants;
+    return v;
 }
 
 - ( NSArray< NSString * > * )commonSubstitutions
 {
-    return @[ self ];
+    {
+        static dispatch_once_t once;
+        
+        dispatch_once
+        (
+            &once,
+            ^( void )
+            {
+                NSString * k;
+                
+                variants =
+                @{
+                    @"A": @[ @"4", @"@", @"^", @"Д" ],
+                    @"B": @[ @"8", @"ß", @"6" ],
+                    @"C": @[ @"[", @"¢", @"[", @"<", @"(", @"©" ],
+                    @"D": @[ @")", @"?", @">" ],
+                    @"E": @[ @"3", @"&", @"£", @"€", @"ë" ],
+                    @"F": @[ @"ƒ", @"v" ],
+                    @"G": @[ @"&", @"6", @"9", @"[" ],
+                    @"H": @[ @"#" ],
+                    @"I": @[ @"1", @"|", @"!" ],
+                    @"J": @[ @";", @"1" ],
+                    @"K": @[],
+                    @"L": @[ @"1", @"£", @"7", @"|" ],
+                    @"M": @[],
+                    @"N": @[ @"И", @"^", @"ท" ],
+                    @"O": @[ @"0", @"Q", @"p", @"Ø" ],
+                    @"P": @[ @"9" ],
+                    @"Q": @[ @"9", @"2", @"&" ],
+                    @"R": @[ @"®", @"Я" ],
+                    @"S": @[ @"5", @"$", @"z", @"§", @"2" ],
+                    @"T": @[ @"7", @"+", @"†" ],
+                    @"U": @[ @"v", @"µ", @"บ" ],
+                    @"V": @[],
+                    @"W": @[ @"Ш", @"Щ", @"พ" ],
+                    @"X": @[ @"Ж", @"×" ],
+                    @"Y": @[ @"j", @"Ч", @"7", @"¥" ],
+                    @"Z": @[ @"2", @"%", @"s" ]
+                }
+                .mutableCopy;
+                    
+                for( k in variants.allKeys )
+                {
+                    [ variants setObject: variants[ k ] forKey: k.lowercaseString ];
+                }
+            }
+        );
+    }
+    
+    if( self.length == 0 )
+    {
+        return @[ @"" ];
+    }
+    
+    if( self.length > 6 )
+    {
+        return @[ self ];
+    }
+    
+    {
+        NSString                     * c;
+        NSString                     * tv;
+        NSString                     * v;
+        NSMutableArray< NSString * > * l;
+        
+        c = [ self substringToIndex: 1 ];
+        l = [ NSMutableArray new ];
+        
+        for( tv in [ self substringFromIndex: 1 ].commonSubstitutions )
+        {
+            [ l addObject: [ c stringByAppendingString: tv ] ];
+            
+            if( variants[ c ].count == 0 )
+            {
+                continue;
+            }
+            
+            for( v in variants[ c ] )
+            {
+                [ l addObject: [ v stringByAppendingString: tv ] ];
+            }
+        }
+        
+        return l;
+    }
 }
 
 @end
